f6b37c0d19fc17f6765f017986020977339a46c2
brachamazuz <93214983+brachamazuz@users.noreply.github.com>
2025-01-12 17:22:32 +0200

    Update dal.js

--- data/dal.js
+++ data/dal.js
@@ -1,10 +1,19 @@
+require('dotenv').config();
+
 const db = require("mysql2");
 
- const pool = db.createPool({
-    host: "localhost",
-    user: "root",
-    database: "landing-page"
- });
+ // const pool = db.createPool({
+ //    host: "localhost",
+ //    user: "root",
+ //    database: "landing-page"
+ // });
+const pool = db.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT || 3306
+});
 
 function executeQueryAsync(sqlCmd, params = []){
     return new Promise((resolve, reject) =>{
